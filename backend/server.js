00af47846715d183ed4a5099d36f7b89089fa327
Sandeep <sandeepkohli180@gmail.com>
2026-09-13 12:10:22 +0530

    Fix cors issue of render url

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 
 //allow origin for security:-
-const allowOrigins = ["http://localhost:5173", "https://kj-sk-leave-management-system.onrender.com"]
+const allowOrigins = ["http://localhost:5173", "https://kj-sk-leave-management-system.onrender.com/api/auth/"]
 app.use(cors({origin: allowOrigins, methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type", "Auhtorization"]}));   //Allow diffenet orgins
 
 connectDB();      //Database connection 
